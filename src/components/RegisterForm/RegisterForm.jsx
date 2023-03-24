252f5cb8fe6160459ef3ff3b5dd49551ad3642fb
SerhiySBV <serhiybelyavskii@gmail.com>
2023-03-25 00:09:52 +0200

    Added styles

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -17,40 +17,52 @@ export const RegisterForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit} autoComplete="off">
-      <label>
-        Username
-        <input
-          id="outlined-basic"
-          label="Name"
-          variant="outlined"
-          type="text"
-          name="name"
-        />
-      </label>
-      <label>
-        Email
-        <input
-          id="outlined-basic"
-          label="Email"
-          variant="outlined"
-          type="email"
-          name="email"
-        />
-      </label>
-      <label>
-        Password
-        <input
-          id="outlined-basic"
-          label="Password"
-          variant="outlined"
-          type="password"
-          name="password"
-        />
-      </label>
-      <button variant="contained" size="large" type="submit">
-        Register
-      </button>
-    </form>
+    <div>
+      <form
+        onSubmit={handleSubmit}
+        style={{
+          display: 'grid',
+          justifyContent: 'center',
+          gridAutoColumns: '183px',
+        }}
+      >
+        <label>
+          Username
+          <input
+            id="outlined-basic"
+            label="Name"
+            variant="outlined"
+            type="text"
+            name="name"
+            placeholder="Name Name"
+          />
+        </label>
+        <label>
+          Email
+          <input
+            id="outlined-basic"
+            label="Email"
+            variant="outlined"
+            type="email"
+            name="email"
+            placeholder="amail@mail.com"
+          />
+        </label>
+        <label>
+          Password
+          <input
+            id="outlined-basic"
+            label="Password"
+            variant="outlined"
+            type="password"
+            name="password"
+            placeholder="password"
+          />
+        </label>
+        <button variant="contained" size="large" type="submit">
+          Register
+        </button>
+      </form>
+    </div>
   );
 };
